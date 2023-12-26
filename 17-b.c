3b86c375028710ad5f3f7067b082da3ee39e53b0
Kunal-Basu-DU <151607882+Kunal-Basu-DU@users.noreply.github.com>
2023-12-26 15:02:31 +0530

    Add files via upload

---
+++ 17-b.c
@@ -0,0 +1,20 @@
+//Program to read data from a file.
+#include <stdio.h>
+int main() {
+FILE *file;
+char data[100];
+
+file = fopen("example.txt", "r");
+
+if (file == NULL) {
+printf("Error opening the file.\n");
+return 1;
+}
+fscanf(file, "%[^\n]", data);
+
+fclose(file);
+
+printf("Data read from the file:\n%s\n", data);
+return 0;
+
+}
