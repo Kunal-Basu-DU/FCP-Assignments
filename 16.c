75985bb10c22747c0eb82b380fd5df51e2e2e9a8
Kunal-Basu-DU <151607882+Kunal-Basu-DU@users.noreply.github.com>
2023-12-26 15:01:26 +0530

    Add files via upload

---
+++ 16.c
@@ -0,0 +1,26 @@
+// FIbonacci series using recursion
+#include<stdio.h>
+int f(int n);
+
+int main()
+{
+    int n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", f(i));
+    }
+
+    return 0;
+}
+int f(int n)
+{
+    if (n < 2)
+        return n;
+    else
+        return f(n - 1) + f(n - 2);
+}
+
